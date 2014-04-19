fc1530aaeabeea977267181cf2ac32aa32540ceb
tummychow <tummychow@users.noreply.github.com>
2014-04-19 18:41:37 -0400

    Implement basic functionality
    The script is in a working condition. Features are coming up!

--- rgdg.rb
+++ rgdg.rb
@@ -1,2 +1,18 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
+
+# Gem::Specification.find_all_by_name("jekyll", Gem::Requirement.new("~> 1.5.1")).each { |spec| p spec.dependencies }
+
+puts "digraph G {"
+
+Gem::Specification.each do |gemspec|
+#  puts "#{gemspec.name} version #{gemspec.version}"
+  gemspec.dependencies.each do |gemdep|
+    if gemdep.type == :runtime
+      puts "    \"#{gemspec.name}\" -> \"#{gemdep.name}\""
+    end
+  end
+end
+
+puts "}"
